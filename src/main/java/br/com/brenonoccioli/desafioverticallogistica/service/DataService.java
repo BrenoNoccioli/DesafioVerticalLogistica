38a8afeffea602f8dd1c88f83ae9503bb101cc71
brenonoccioli <brenonoccioli@gmail.com>
2024-04-28 15:21:44 -0300

    fluxo de persistecia finalizado

--- src/main/java/br/com/brenonoccioli/desafioverticallogistica/service/DataService.java
+++ src/main/java/br/com/brenonoccioli/desafioverticallogistica/service/DataService.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.*;
@@ -22,6 +23,7 @@ import static br.com.brenonoccioli.desafioverticallogistica.helpers.ProcessDataH
 @RequiredArgsConstructor
 public class DataService {
     private final UsersRepository usersRepository;
+    private final OrdersRepository ordersRepository;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DataService.class);
 
@@ -31,83 +33,80 @@ public class DataService {
         Scanner scanner = new Scanner(dataArchive);
 
         ArrayList<String> errorList = new ArrayList<>();
-        ArrayList<UserEntity> newUserEntities = new ArrayList<>();
+        ArrayList<UserEntity> usersForPersistence = new ArrayList<>();
+        ArrayList<OrderEntity> ordersForPersistence = new ArrayList<>();
 
         while (scanner.hasNext()){
             String line = scanner.nextLine();
-            LOGGER.info(String.format("Linha: %s", line));
 
             if (lineIsValid(line)){
-                UserEntity newUser = buildUserFromLineFields(line);
-                OrderEntity newOrder = buildOrderFromLineFields(line);
-                Product newProduct = buildProductFromLineFields(line);
+                UserEntity newUser = buildUserFromLine(line);
+                OrderEntity newOrder = buildOrderFromLine(line);
+                Product newProduct = buildProductFromLine(line);
 
                 if (newUser == null || newOrder == null || newProduct == null){
                     errorList.add(line);
                     continue;
                 }
 
-                UserEntity existingUser = getExistingUser(newUserEntities, newUser);
+                OrderEntity existingOrder = getExistingOrder(ordersForPersistence, newOrder);
 
-                if (existingUser != null){
-                    OrderEntity existingOrder = getExistingOrder(existingUser.getOrders(), newOrder);
-                    if (existingOrder != null){
-                        existingOrder.getProducts().add(newProduct);
-                    } else {
-                        newOrder.getProducts().add(newProduct);
-                        existingUser.getOrders().add(newOrder);
-                    }
+                if (existingOrder != null){
+                    existingOrder.getProducts().add(newProduct);
                 } else {
                     newOrder.getProducts().add(newProduct);
-                    newUser.getOrders().add(newOrder);
-                    newUserEntities.add(newUser);
+                    ordersForPersistence.add(newOrder);
                 }
+
+                if (!usersForPersistence.contains(newUser)){
+                    usersForPersistence.add(newUser);
+                }
+
             } else {
                 errorList.add(line);
             }
         }
 
-        persistOrUpdateNewData(newUserEntities);
+        persistAndUpdate(usersForPersistence, ordersForPersistence);
 
         if (errorList.size() > 0){
-            LOGGER.error(String.format("Linhas inválidas do arquivo: %s", errorList));
+            LOGGER.error(String.format("Arquivo processado parcialmente com sucesso." +
+                    "\nLinhas não processadas:\n%s", errorList));
         } else {
             LOGGER.info("Arquivo processado inteiramente com sucesso!");
         }
 
     }
 
-    private void persistOrUpdateNewData(ArrayList<UserEntity> newUserEntities) {
-        for (UserEntity newUser : newUserEntities){
-            Set<OrderEntity> newOrdersList = newUser.getOrders();
+    @Transactional
+    private void persistAndUpdate(List<UserEntity> newUserList, List<OrderEntity> newOrderList) {
+        ArrayList<UserEntity> userPersistList = new ArrayList<>();
+        for (UserEntity newUser : newUserList){
             Optional<UserEntity> optionalUser = usersRepository.findById(newUser.getId());
 
-            if (optionalUser.isPresent()){
-                UserEntity oldUser = optionalUser.get();
-                Set<OrderEntity> oldOrdersList = oldUser.getOrders();
-
-                oldOrdersList.forEach(oldOrder -> {
-                    newOrdersList.forEach(newOrder -> {
-                        if (oldOrder.getId().equals(newOrder.getId())){
-
-                            oldOrder.setTotalPrice(calculateTotalPrice(oldOrder.getProducts()));
-                            oldOrder.getProducts().addAll(newOrder.getProducts());
-                        } else {
-                            oldOrder.setTotalPrice(calculateTotalPrice(oldOrder.getProducts()));
-                            oldOrdersList.add(newOrder);
-                        }
-                    });
-                });
-                usersRepository.save(oldUser);
-            } else {
-                newUser.getOrders().forEach(
-                        order -> order.setTotalPrice(calculateTotalPrice(order.getProducts())));
-                usersRepository.save(newUser);
+            if (optionalUser.isEmpty()){
+                userPersistList.add(newUser);
             }
         }
+        usersRepository.saveAll(userPersistList);
+
+        HashSet<OrderEntity> orderPersistList = new HashSet<>();
+        for (OrderEntity newOrder : newOrderList){
+            Optional<OrderEntity> optionalOrder = ordersRepository.findById(newOrder.getId());
+
+            if (optionalOrder.isPresent()){
+                OrderEntity oldOrder = optionalOrder.get();
+                oldOrder.getProducts().addAll(newOrder.getProducts());
+                orderPersistList.add(oldOrder);
+            } else {
+                orderPersistList.add(newOrder);
+            }
+        }
+        orderPersistList.forEach(order -> order.setTotalPrice(calculateTotalPrice(order.getProducts())));
+        ordersRepository.saveAll(orderPersistList);
     }
 
-    private Product buildProductFromLineFields(String line) {
+    private Product buildProductFromLine(String line) {
         Long productId = getProductId(line);
         BigDecimal productValue = getProductValue(line);
 
@@ -121,9 +120,10 @@ public class DataService {
                 .build();
     }
 
-    private OrderEntity buildOrderFromLineFields(String line) {
+    private OrderEntity buildOrderFromLine(String line) {
         Long orderId = getOrderId(line);
         String orderStrDate = getOrderDate(line);
+        UserEntity user = buildUserFromLine(line);
 
         if (orderId == null || orderStrDate == null){
             return null;
@@ -134,10 +134,11 @@ public class DataService {
                 .date(convertStringtoLocalDate(orderStrDate, "yyyMMdd"))
                 .totalPrice(BigDecimal.ZERO)
                 .products(new ArrayList<>())
+                .user(user)
                 .build();
     }
 
-    private UserEntity buildUserFromLineFields(String line) {
+    private UserEntity buildUserFromLine(String line) {
         Long userId = getUserId(line);
         String userName = getUserName(line);
 
@@ -152,19 +153,10 @@ public class DataService {
                 .build();
     }
 
-    private OrderEntity getExistingOrder(Set<OrderEntity> orderList, OrderEntity newOrder){
+    private OrderEntity getExistingOrder(List<OrderEntity> orderList, OrderEntity newOrder){
         for (OrderEntity order : orderList){
             if (order.getId().equals(newOrder.getId())){
                 return order;
-            }
-        }
-        return null;
-    }
-
-    private UserEntity getExistingUser(List<UserEntity> userList, UserEntity newUser){
-        for (UserEntity user : userList){
-            if (user.getId().equals(newUser.getId())){
-                return user;
             }
         }
         return null;
